72cc724434e2884aee903cd9206e83a6c26b1fda
Pedro Reyes <pedro.reyes.solutions@gmail.com>
2023-10-10 16:17:43 +0200

    release/v0.0.1

---
+++ bloc/release/v0.0.1/deploy.ts
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+
+import { mainTask0001_Deploy } from "./deployCircle";
+
+/**
+✍ Script for deploying this strategy
+
+$ npx hardhat run release/next_release/deploy.ts --network bscTestnet
+*/
+async function main() {
+    await mainTask0001_Deploy();
+    // await mainTask0002_Deploy();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
